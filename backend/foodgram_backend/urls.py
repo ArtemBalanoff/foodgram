d49ae45062867f5905734eb2bb6960f6d4241fa1
Artem Balanov <artembalanoff@mail.ru>
2024-10-03 20:48:03 +0300

    .

--- backend/foodgram_backend/urls.py
+++ backend/foodgram_backend/urls.py
@@ -1,7 +1,15 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.shortcuts import get_object_or_404, redirect
+
+
+def recipe_redirect(request, short_link):
+    recipe = get_object_or_404('recipes.Recipe', short_link=short_link)
+    return redirect(f'recipes/{recipe.id}/')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('s/<str:short_link>/', recipe_redirect),
     path('api/', include('api.urls')),
 ]
